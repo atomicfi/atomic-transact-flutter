e07a8d37de3385b8e03db7143889844e1778e834
Sean Hill <sean.daryl.hill@gmail.com>
2025-02-03 09:53:44 -0700

    chore: update android example app to latest android deps

--- example/android/app/src/main/kotlin/atomic/financial/atomic_transact_flutter_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/atomic/financial/atomic_transact_flutter_example/MainActivity.kt
@@ -2,5 +2,4 @@ package atomic.financial.atomic_transact_flutter_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
