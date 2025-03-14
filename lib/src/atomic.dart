5eada8521838899d45605be441ab281bb5cb7a07
Mark Gowarty-Weinstock <80653513+MarkGDub@users.noreply.github.com>
2025-03-14 13:28:21 -0600

    feat: update atomic class to prevent duplicate transact instances (#16)

--- lib/src/atomic.dart
+++ lib/src/atomic.dart
@@ -1,9 +1,11 @@
 import '../platform_interface/atomic_platform_interface.dart';
+import '../src/events.dart';
 
 import 'types.dart';
 import 'config.dart';
 
 class Atomic {
+  static bool _isLoading = false;
   static AtomicPlatformInterface get _platform =>
       AtomicPlatformInterface.instance;
 
@@ -21,11 +23,25 @@ class Atomic {
     AtomicTaskStatusUpdateHandler? onTaskStatusUpdate,
     AtomicCompletionHandler? onCompletion,
   }) async {
+    if (_isLoading) {
+      return;
+    }
+    _isLoading = true;
+
     _platform.onInteraction = onInteraction;
     _platform.onDataRequest = onDataRequest;
     _platform.onAuthStatusUpdate = onAuthStatusUpdate;
     _platform.onTaskStatusUpdate = onTaskStatusUpdate;
-    _platform.onCompletion = onCompletion;
+    _platform.onCompletion = (
+      AtomicTransactCompletionType type,
+      AtomicTransactResponse? response,
+      AtomicTransactError? error,
+    ) {
+      _isLoading = false;
+      if (onCompletion != null) {
+        return onCompletion(type, response, error);
+      }
+    };
 
     await _platform.presentTransact(
       configuration: config,
@@ -41,14 +57,21 @@ class Atomic {
     AtomicTaskStatusUpdateHandler? onTaskStatusUpdate,
     AtomicCompletionHandler? onCompletion,
   }) async {
-    _platform.onLaunch = onLaunch;
+    if (_isLoading) {
+      return;
+    }
+    _isLoading = true;
+
+    _platform.onLaunch = () {
+      _isLoading = false;
+      if (onLaunch != null) {
+        return onLaunch();
+      }
+    };
     _platform.onAuthStatusUpdate = onAuthStatusUpdate;
     _platform.onTaskStatusUpdate = onTaskStatusUpdate;
     _platform.onCompletion = onCompletion;
 
-    await _platform.presentAction(
-      id: id,
-      environment: environment,
-    );
+    await _platform.presentAction(id: id, environment: environment);
   }
 }
